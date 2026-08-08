3b2510d6303fe23323194df28077bfb1062ecd1d
Zhong Cheng <zctiong@u.nus.edu>
2026-08-08 12:27:24 +0800

    fix: format security hub terraform root

--- infra/terraform/securityhub-import/main.tf
+++ infra/terraform/securityhub-import/main.tf
@@ -27,9 +27,9 @@ resource "aws_iam_role" "sonar_securityhub_import" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Effect = "Allow"
+      Effect    = "Allow"
       Principal = { Federated = "arn:aws:iam::${var.expected_account_id}:oidc-provider/token.actions.githubusercontent.com" }
-      Action = "sts:AssumeRoleWithWebIdentity"
+      Action    = "sts:AssumeRoleWithWebIdentity"
       Condition = { StringEquals = {
         "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         "token.actions.githubusercontent.com:sub" = var.github_oidc_main_subject
